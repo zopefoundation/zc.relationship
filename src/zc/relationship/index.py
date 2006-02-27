37a0215da3922eff6e6c1d7cc00faceefbf11ef3
Gary Poster <gary@zope.com>
2006-02-27 23:00:30 +0000

    No longer using partial.

--- src/zc/relationship/index.py
+++ src/zc/relationship/index.py
@@ -1,20 +1,4 @@
 import types
-try:
-    from functional import partial # Py 2.5
-except ImportError:
-    class partial(object): # from http://www.python.org/peps/pep-0309.html
-
-        def __init__(*args, **kw):
-            self = args[0]
-            self.fn, self.args, self.kw = (args[1], args[2:], kw)
-
-        def __call__(self, *args, **kw):
-            if kw and self.kw:
-                d = self.kw.copy()
-                d.update(kw)
-            else:
-                d = kw or self.kw
-            return self.fn(*(self.args + args), **d)
 
 import persistent
 import persistent.interfaces
