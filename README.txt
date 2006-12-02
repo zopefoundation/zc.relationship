a2dc96b054217376eb9a81ca4f7b8dacccc60803
Gary Poster <gary@modernsongs.com>
2006-12-02 03:02:34 +0000

    Make this a zc.buildout package

---
+++ README.txt
@@ -0,0 +1,2 @@
+Low-level ZODB relationship index: supports intransitive and transitive n-ary
+relationships.  Example usage of "relationship containers".
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.relationship",
+    version="1.0",
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir= {'':'src'},
+    
+    namespace_packages=['zc'],
+
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description=open("README.txt").read(),
+    long_description=open("src/zc/relationship/README.txt").read(),
+    license='ZPL 2.1',
+    keywords="zope zope3",
+    )
