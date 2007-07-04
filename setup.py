d88c6bc9b9d472c62fadffc6c7a0bca9c11928dc
Gary Poster <gary@modernsongs.com>
2007-07-04 15:54:29 +0000

    convert branch to eggs; prepare for 1.1 release; update CHANGES

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zc.relationship",
-    version="1.1a",
+    version="1.1",
     packages=find_packages('src'),
     include_package_data=True,
     package_dir= {'':'src'},
@@ -14,8 +14,24 @@ setup(
     author_email='zope3-dev@zope.org',
     description=open("README.txt").read(),
     long_description=(
-        open('CHANGES.txt').read() + '\n========\nOverview\n========\n\n' +
+        open('CHANGES.txt').read() +
+        '\n========\nOverview\n========\n\n' +
         open("src/zc/relationship/README.txt").read()),
     license='ZPL 2.1',
     keywords="zope zope3",
+    install_requires=[
+        'ZODB3 >= 3.8dev',
+        'zope.app.container', # would be nice to remove this
+        'zope.app.intid',
+        'zope.interface',
+        'zope.component',
+        'zope.app.keyreference',
+        'zope.location',
+        'zope.index',
+        
+        'zope.app.testing', # TODO remove this
+        'zope.app.component', # TODO remove this
+        'zope.testing',
+        'setuptools',
+        ],
     )
