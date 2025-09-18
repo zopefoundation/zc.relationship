0d382b920c7932770f18467d6c1b5c4563f80c73
Wolfgang Schnerring <wosc@wosc.de>
2025-09-18 09:14:10 +0200

    Switch to PEP 420 native namespace. (#13)
    * Configuring for pure-python
    
    * Replace ``pkg_resources`` namespace with PEP 420 native namespace.
    
    * Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@
 ##############################################################################
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -26,10 +25,7 @@ def read(path):
 setup(
     name="zc.relationship",
     version='3.0.dev0',
-    packages=find_packages('src'),
     include_package_data=True,
-    package_dir={'': 'src'},
-    namespace_packages=['zc'],
     zip_safe=False,
     author='Gary Poster',
     author_email='zope-dev@zope.dev',
